52807bceb12491c774c4a87c5ecca0e5b02a2286
Joshy Cyriac <j.cyriac@gmail.com>
2018-01-09 17:22:16 +0100

    lower bottom margin, only needed for additional xaxis

--- repo/static/js/script.js
+++ repo/static/js/script.js
@@ -73,7 +73,7 @@ $(function () {
 
     function draw_grouped() {
         var svg = d3.select("#grouped"),
-            margin = { top: 20, right: 20, bottom: 50, left: 40 },
+            margin = { top: 20, right: 20, bottom: 20, left: 40 },
             width = +svg.attr("width") - margin.left - margin.right,
             height = +svg.attr("height") - margin.top - margin.bottom,
             g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
