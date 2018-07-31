a97d77b5b134255eff9fcd5fe54d0abeeed9c6f0
Joshy Cyriac <j.cyriac@gmail.com>
2018-07-31 11:23:23 +0200

    better be safe

--- distiller/__init__.py
+++ distiller/__init__.py
@@ -2,7 +2,9 @@ from distiller.calcium import extract_score
 from distiller.ventricle_function import extract_ventricle_function
 
 def process(report, meta_data):
-    if meta_data['Untersuchung'] == 'CT Herz':
+    if meta_data is None:
+        return {}
+    elif meta_data['Untersuchung'] == 'CT Herz':
         return extract_score(report, meta_data)
     elif meta_data['Untersuchung'] == 'MRI Herz':
         return extract_ventricle_function(report, meta_data)
