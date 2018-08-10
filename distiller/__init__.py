2e933255f00411f2eb65e129c659d788a66c7805
Joshy Cyriac <j.cyriac@gmail.com>
2018-08-10 18:02:46 +0200

    started work on aorta table

--- distiller/__init__.py
+++ distiller/__init__.py
@@ -1,11 +1,14 @@
 from distiller.calcium import extract_score
+from distiller.aorta import extract_table
 from distiller.ventricle_function import extract_ventricle_function
 
 def process(report, meta_data):
     if meta_data is None:
         return {}
     elif meta_data['Untersuchung'] == 'CT Herz':
-        return extract_score(report, meta_data)
+        x = extract_score(report, meta_data)
+        y = extract_table(report, meta_data)
+        return {**x, **y}
     elif meta_data['Untersuchung'] == 'MRI Herz':
         return extract_ventricle_function(report, meta_data)
     else:
---
+++ distiller/aorta.py
@@ -0,0 +1,25 @@
+"""
+Extract aorta score from report
+"""
+
+RESULT_AORTA = 'aorta'
+
+def extract_table(report, meta_data):
+    result = {}
+    lines = [s.strip() for s in report.splitlines()]
+    for l in lines:
+        if l.startswith('Anulus'):
+            result['Anulus'] = l.split('|')[1]
+        if l.startswith('Sinus'):
+            result['Sinus'] = l.split('|')[1]
+        if l.startswith('Sinutubulärer'):
+            result['Sinutubulärer'] = l.split('|')[1]
+        if l.startswith('Aszendens'):
+            result['Aszendens'] = l.split('|')[1]
+        if l.startswith('Asz'):
+            result['Asz'] = l.split('|')[1]
+        if l.startswith('Bogen zw'):
+            result['Bogen zw'] = l.split('|')[1]
+        if l.startswith('Bogen distal'):
+            result['Bogen distal'] = l.split('|')[1]
+    return {'aorta': result}
