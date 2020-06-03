c0b2e9b2653b427b158a36e5fde78243091ca07c
Joshy Cyriac <j.cyriac@gmail.com>
2020-06-03 16:33:44 +0200

    formatted black

--- runserver.py
+++ runserver.py
@@ -2,21 +2,24 @@ import os
 import logging
 import daiquiri
 
-LOG_DIR = 'logs'
+LOG_DIR = "logs"
 
 if not os.path.exists(LOG_DIR):
     os.makedirs(LOG_DIR)
 
-daiquiri.setup(level=logging.DEBUG,
+daiquiri.setup(
+    level=logging.DEBUG,
     outputs=(
-        daiquiri.output.File('logs/review-errors.log', level=logging.ERROR),
+        daiquiri.output.File("logs/review-errors.log", level=logging.ERROR),
         daiquiri.output.RotatingFile(
-            'logs/review-debug.log',
+            "logs/review-debug.log",
             level=logging.DEBUG,
             # 10 MB
-            max_size_bytes=10000000)
-    ))
+            max_size_bytes=10000000,
+        ),
+    ),
+)
 
 from review.app import app
 
-app.run(host='0.0.0.0', port=5555)
+app.run(host="0.0.0.0", port=5555)
