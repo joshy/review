dafef9c33281b1b0a0317799895c50e5452d90fc
Joshy Cyriac <joshy@posteo.ch>
2020-10-20 15:15:08 +0200

    Update connection.py
    fixed connection

--- repo/database/connection.py
+++ repo/database/connection.py
@@ -4,6 +4,6 @@ import cx_Oracle
 
 def open_connection(host, port, service, user, password):
     logging.debug('Opening connection to ris db')
-    dsn = cx_Oracle.makedsn(host, port, )
+    dsn = cx_Oracle.makedsn(host, port, service_name=service)
     con = cx_Oracle.connect(user, password, dsn)
     return con
