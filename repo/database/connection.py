89bb3d30f596a27207e2921557dff18ee033f555
Joshy Cyriac <j.cyriac@gmail.com>
2022-12-20 11:48:50 +0100

    cleanup

--- repo/database/connection.py
+++
@@ -1,9 +0,0 @@
-import logging
-import cx_Oracle
-
-
-def open_connection(host, port, service, user, password):
-    logging.debug('Opening connection to ris db')
-    dsn = cx_Oracle.makedsn(host, port, service_name=service)
-    con = cx_Oracle.connect(user, password, dsn)
-    return con
--- review/database.py
+++ review/database.py
@@ -1,34 +1,88 @@
 import logging
 import psycopg2
 
+from jinja2 import Template
 
-def query_review_reports(cursor):
+
+def query_review_report_by_acc(cursor, id):
+    sql = """
+          SELECT
+            *
+          FROM
+            sectra_reports a
+          WHERE
+              a.accession_number = %s
+          """
+    cursor.execute(sql, (id,))
+    desc = [d[0].lower() for d in cursor.description]
+    result = [dict(zip(desc, row)) for row in cursor]
+    return result[0] if result else []
+
+
+def query_review_report(cursor, id):
+    sql = """
+          SELECT
+            *
+          FROM
+            sectra_reports a
+          WHERE
+              a.accession_number = %s
+          """
+    cursor.execute(sql, (id,))
+    desc = [d[0].lower() for d in cursor.description]
+    result = [dict(zip(desc, row)) for row in cursor]
+    return result[0] if result else []
+
+
+def query_review_reports(cursor, day, writer, reviewer, report_status):
     """
-    Returns the rows where the reports are finalized and metrics are not yet
-    calculated.
+    Query all reports in the review db by day and writer (optional) and 
+    reviewer (optional) and befund status (optional).
     """
     sql = """
           SELECT
-            accession_number,
-            report_s,
-            report_v,
-            report_f,
-            unters_beginn
+            a.pid,
+            a.accession_number,
+            a.unters_beginn,
+            a.untart_kuerzel,
+            a.untart_name,
+            a.schreiber,
+            a.vor_signierer,
+            a.fin_signierer,
+            a.report_status,
+            a.untart_name,
+            a.jaccard_v_f,
+            a.jaccard_s_f,
+            a.words_added_v_f,
+            a.words_deleted_v_f,
+            a.modality
           FROM
-            sectra_reports
+            sectra_reports a
           WHERE
-            report_f is not null
-          AND
-            jaccard_s_f is null
-          OR
-            (jaccard_s_f = 0 AND jaccard_v_f = 0)
+              a.unters_beginn
+                BETWEEN
+                  %s
+                    AND
+                  %s
+            {{ other_clause }}
           ORDER BY
-            unters_beginn desc
-          LIMIT 1000
+              a.unters_beginn
           """
-    cursor.execute(sql)
-    results = cursor.fetchall()
-    return results
+    start = day.strftime("%Y-%m-%d 00:00:00")
+    end = day.strftime("%Y-%m-%d 23:59:59")
+    template = Template(sql)
+    sql = ""
+    if writer:
+        sql += f" AND a.schreiber = '{writer.lower()}'"
+    if reviewer:
+        sql += f" AND a.fin_signierer = '{reviewer.lower()}'"
+    if report_status:
+        sql += f" AND a.report_status = '{report_status.lower()}'"
+    sql = template.render(other_clause=sql)
+    cursor.execute(sql, (start, end))
+    desc = [d[0].lower() for d in cursor.description]
+    result = [dict(zip(desc, row)) for row in cursor]
+    return result
 
 
 def update_metrics(cursor, accession_number, diffs):
