f149d874bccc02683cb3bcd262a32f66074509b9
Joshy Cyriac <j.cyriac@gmail.com>
2023-12-01 18:21:51 +0100

    formatter

--- review/calculations.py
+++ review/calculations.py
@@ -3,23 +3,29 @@ from math import floor
 
 def relative(df):
     if df.shape[0] > 0:
-        df['words_added_relative_v_f'] = df.apply(
-            lambda row: _relative(row['total_words_f'], row['words_added_v_f']), axis=1)
-        df['words_added_relative_s_f'] = df.apply(
-            lambda row: _relative(row['total_words_f'], row['words_added_s_f']), axis=1)
-        df['words_deleted_relative_v_f'] = df.apply(
-            lambda row: _relative(row['total_words_f'], row['words_deleted_v_f']), axis=1)
-        df['words_deleted_relative_s_f'] = df.apply(
-            lambda row: _relative(row['total_words_f'], row['words_deleted_s_f']), axis=1)
+        df["words_added_relative_v_f"] = df.apply(
+            lambda row: _relative(row["total_words_f"], row["words_added_v_f"]), axis=1
+        )
+        df["words_added_relative_s_f"] = df.apply(
+            lambda row: _relative(row["total_words_f"], row["words_added_s_f"]), axis=1
+        )
+        df["words_deleted_relative_v_f"] = df.apply(
+            lambda row: _relative(row["total_words_f"], row["words_deleted_v_f"]),
+            axis=1,
+        )
+        df["words_deleted_relative_s_f"] = df.apply(
+            lambda row: _relative(row["total_words_f"], row["words_deleted_s_f"]),
+            axis=1,
+        )
     return df
 
 
 def _relative(total, changes):
-    if total is None or total == 'None':
+    if total is None or total == "None":
         t = 1.0
-    else: 
+    else:
         t = total
-    if changes is None or changes == 'None':
+    if changes is None or changes == "None":
         c = 0
     else:
         c = changes
@@ -30,10 +36,10 @@ def calculate_median_by_writer(df):
     rows = {}
     median = {}
     if df.shape[0] > 0:
-        writer_list = df['schreiber'].drop_duplicates()
+        writer_list = df["schreiber"].drop_duplicates()
         for writer in writer_list:
-            rows[writer] = df.loc[df['schreiber'] == writer].to_dict('records')
-            median['median_' + writer] = calculate_median(rows[writer])
+            rows[writer] = df.loc[df["schreiber"] == writer].to_dict("records")
+            median["median_" + writer] = calculate_median(rows[writer])
     return median
 
 
@@ -41,16 +47,22 @@ def calculate_median_by_reviewer(df):
     rows = {}
     median = {}
     if df.shape[0] > 0:
-        reviewer_list = df['fin_signierer'].drop_duplicates()
+        reviewer_list = df["fin_signierer"].drop_duplicates()
         for reviewer in reviewer_list:
-            rows[reviewer] = df.loc[df['fin_signierer'] == reviewer].to_dict('records')
-            median['median_' + reviewer] = calculate_median(rows[reviewer])
+            rows[reviewer] = df.loc[df["fin_signierer"] == reviewer].to_dict("records")
+            median["median_" + reviewer] = calculate_median(rows[reviewer])
     return median
 
 
 def calculate_median(rows):
-    keys = ["jaccard_s_f", "jaccard_v_f", "words_added_relative_s_f",
-            "words_added_relative_v_f", "words_deleted_relative_s_f", "words_deleted_relative_v_f"]
+    keys = [
+        "jaccard_s_f",
+        "jaccard_v_f",
+        "words_added_relative_s_f",
+        "words_added_relative_v_f",
+        "words_deleted_relative_s_f",
+        "words_deleted_relative_v_f",
+    ]
     median_values = {}
 
     for key in keys:
@@ -63,7 +75,7 @@ def calculate_median(rows):
 def _calculate_median(values):
     if len(values) == 0:
         return 0
-    values = [v for v in values if v != 'None']
+    values = [v for v in values if v != "None"]
     values = sorted(values)
     half = int(floor(len(values) / 2))
     if len(values) == 0:
