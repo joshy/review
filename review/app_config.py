e72f2e4795af2e77a7073aaa1d65c9c26ac631ad
Joshy Cyriac <joshy@posteo.ch>
2025-04-16 22:03:12 +0200

    fix path

--- review/app_config.py
+++ review/app_config.py
@@ -23,7 +23,7 @@ CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
 AUTHORITY = f"https://login.microsoftonline.com/{os.getenv('TENANT_ID', 'common')}"
 REDIRECT_PATH = "/getAToken"
-REDIRECT_URI = f"{os.getenv('SERVER_URL')}/{REDIRECT_PATH}"
+REDIRECT_URI = f"{os.getenv('SERVER_URL')}{REDIRECT_PATH}"
 # This resource requires no admin consent
 ENDPOINT = "https://graph.microsoft.com/v1.0/users"
 SCOPE = ["User.ReadBasic.All"]
